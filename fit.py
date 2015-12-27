8f8152dd1afbc5d808f5988463a82413a88d4b3e
tychon <tychon@tychons.net>
2015-12-27 02:04:35 +0100

    initial commit with working quantity formatting and fitting helper

---
+++ fit.py
@@ -0,0 +1,346 @@
+
+# This module gives easy access to fitting functions and supports
+# the quantities module.
+
+# Known issue, to be investigated:
+#   In test_linear the boostrap algorithm almost always finds a
+#   too shallow slope, whereas ODR gives a better result.
+#   It matches curve_fit for y errors only.
+
+import inspect
+import numpy as np
+import numpy.random as random
+import quantities as pq
+
+import scipy.optimize as opt
+import scipy.odr as odr
+import scipy.stats as stats
+
+from fmt import *
+
+# see http://stackoverflow.com/a/21844726
+def bootstrap(fun, p0, xdata, ydata,
+              xerr=None, yerr=None,
+              runs=1000, pprint=''):
+    """Estimate parameters and their standard deviation from
+      given data with uncertainty using boostrap Monte-Carlo method.
+      Works on pure numbers without units.
+
+      When xerr and yerr are None, scipy's leastsq is used for an initial fit.
+      The standard deviation of residuals is then used to scatter the ydata
+      points for boostrapping.
+
+      Arguments:
+        fun: The model function `ys = f(xs, *params)`
+        p0: Initial parameters.
+        runs: The number of data sets to generate
+        pprint: When bool(pprint) is True some explaining output is generated
+          and printed to stdout prepended with pprint (so you can use it
+          to add indentation).
+
+      Returns: `(params, std)`
+        Where params is a list of fitted parameters and std their
+        standard deviation.
+
+      Raises:
+        RuntimeError: If a leastsq run didn't find a solution.
+    """
+    if pprint: print(pprint+"Bootstrapping with %d runs"%runs)
+    errfunc = lambda p, x, y: fun(x, *p) - y
+    if xerr is None and yerr is None:
+        if pprint: print(pprint+"    Estimating std dev of residuals")
+        params, pcov, info, msg, ier =\
+            opt.leastsq(errfunc, p0, args=(xdata, ydata),
+                        full_output=1)
+        if not (1 <= ier <= 4):
+            raise RuntimeError("Could not estimate residuals: %s"%msg)
+        s_res = np.std(errfunc(params, xdata, ydata))
+        if pprint:
+            print(pprint+"        parameters: "+repr(param))
+            print(pprint+"        std dev of residuals: %f"+s_res)
+    ps = []
+    for i in range(runs):
+        if xerr is None and yerr is None:
+            xs = xdata
+            ys = np.random.normal(ydata, s_res)
+        else:
+            if xerr is not None: xs = random.normal(xdata, xerr)
+            else: xs = xdata
+            if yerr is not None: ys = random.normal(ydata, yerr)
+            else: ys = ydata
+        rparam, rcov, info, msg, ier =\
+            opt.leastsq(errfunc, p0, args=(xs, ys),
+                        full_output=1)
+        if not (1 <= ier <= 4):
+            raise RuntimeError("No solution found for all data sets: %s"%s)
+        ps.append(rparam)
+    ps = np.array(ps)
+    params = np.mean(ps, 0)
+    std = np.std(ps, 0)
+    if pprint:
+        print(pprint+"    parameters: "+repr(params))
+        print(pprint+"    std dev: "+repr(std))
+    return (params, std)
+
+def fit(fun, xdata, ydata, p0,
+        xerr=None, yerr=None,
+        usebootstrap=False, pprint=True):
+    """This is a helper function dealing with the quantities and is handling
+      over the real math to other fit functions.
+
+      Arguments:
+        xdata: The x data points as Quantity or simple ndarray (dimensionless).
+        ydata: The y data points as Quantity or simple ndarray (dimensionless),
+            Has to have same length as xdata.
+        p0: Initial parameters, used to determine number of free parameters when
+            present.  The model function will receive the paramesters in the
+            same units, and results will be displayed in these units.
+        xerr: Uncertainty for xdata, either a single value for all points, or
+            an array of same length as xdata.  Will be rescaled to same unit as
+            xdata before fitting.
+        yerr: Uncertainty for ydata.
+        usebootstrap: Use bootstrapping instead of some scipy fitting algorithm.
+            Bootstrapping works for all types of uncertainties.
+        pprint: Set True to see some more information about fit.
+
+      Returns:
+        params: A list of parameters as pq.Quantity
+        std: Standard deviations of parameters in same units
+        info: A dict with the following keys:
+          'method': 'curve_fit' or 'odr'
+          'ok': True when the fit was probably successful
+          'cov' (optional): the covariance matrix
+          'res_var': residual variance / reduced chi squared
+          'msg' (optional): some information about fit in english
+    """
+    assert len(np.shape(xdata)) is 1
+    assert len(np.shape(ydata)) is 1
+    n = len(xdata)
+    assert n == len(ydata)
+    dof = n - len(p0)
+    assert dof > 0
+    # inspect model function for parameter names
+    argspec = inspect.getargspec(fun)
+    paramnames = None
+    if argspec.varargs is None:
+        if len(p0) != len(argspec.args) - 1:
+            raise ValueError("Number of arguments of model function"
+                             " is not matching length of p0.")
+        paramnames = argspec.args[1:]
+    else:
+        paramnames = ["param%d"%(i+1) for i in range(dof)]
+    # strip units
+    assert isinstance(xdata, np.ndarray)
+    assert isinstance(ydata, np.ndarray)
+    if isinstance(xdata, pq.Quantity):
+        xunits = xdata.units
+        xdata = xdata.magnitude
+    else: xunits = pq.dimensionless
+    if isinstance(ydata, pq.Quantity):
+        yunits = ydata.units
+        ydata = ydata.magnitude
+    else: yunits = pq.dimensionless
+    if xerr is not None:
+        if isinstance(xerr, pq.Quantity):
+            xerr = xerr.rescale(xunits).magnitude
+        assert len(np.shape(xerr)) == 0 or np.shape(xerr) == (len(xdata))
+        if np.amin(xerr) <= 0:
+            raise ValueError("Zeros / neg values in x-uncert not allowed")
+    if yerr is not None:
+        if isinstance(yerr, pq.Quantity):
+            yerr = yerr.rescale(yunits).magnitude
+        assert len(np.shape(yerr)) == 0 or np.shape(yerr) == (len(ydata))
+        if np.amin(yerr) <= 0:
+            raise ValueError("Zeros / neg values in y-uncert not allowed")
+    p0 = [p if isinstance(p, pq.Quantity) else p * pq.dimensionless
+                for p in p0]
+    p0units = [p.units for p in p0]
+    p0 = [p.magnitude for p in p0]
+    # wrap function appending units again
+    def func(x, *params):
+        params2 = [p*u for p,u in zip(params, p0units)]
+        return fun(x * xunits, *params2).rescale(yunits).magnitude
+
+    # fit
+    if pprint:
+        print("Fitting %s depending on %s"%(repr(yunits)[13:],
+                                            repr(xunits)[13:]))
+        print("    N=%d, params=%d, dof=%d"%(n, len(p0), dof))
+    params = None # estimated parameters
+    std = None    # standard deviation
+    info = {}     # additional information
+    if usebootstrap:
+        if pprint: print("    Method: bootstrap")
+        info['method'] = 'bootstrap'
+        try:
+            if pprint: ppprint = '    '
+            else: ppprint = False
+            params, std = bootstrap(fun, p0, xdata, ydata, xerr, yerr,
+                                    pprint=ppprint)
+        except RuntimeError as e:
+            info['ok'] = False
+            info['msg'] = str(e)
+        else:
+            info['ok'] = True
+            if xerr is None and yerr is None:
+                info['res_var'] = np.sum((func(xdata, *params)-ydata)**2) / dof
+            elif xerr is None:
+                info['res_var'] = (np.sum((func(xdata, *params)-ydata)**2
+                                         / yerr**2) / dof)
+    elif xerr is None and yerr is None:
+        if pprint: print("    Method: scipy leastsq")
+        info['method'] = 'leastsq'
+        errfunc = lambda p, x, y: fun(x, *p) - y
+        params, pcov, inf, msg, ier =\
+            opt.leastsq(errfunc, p0,
+                        args=(xdata, ydata),
+                        full_output=1)
+        if not (1 <= ier <= 4):
+            info['ok'] = False
+            info['msg'] = msg
+        else:
+            info['ok'] = pcov is not None
+            info['res_var'] = np.sum((func(xdata, *params)-ydata)**2) / dof
+            info['msg'] = msg
+            if pcov is not None:
+                # multiply by residual variance because leastsq
+                # returns the reduced covariance matrix
+                info['cov'] = pcov * info['res_var']
+                std = [np.sqrt(pcov[i][i]) for i in range(len(p0))]
+            else: std = [np.inf] * len(p0)
+    elif xerr is None:
+        if pprint: print("    Method: scipy curve_fit")
+        info['method'] = 'curve_fit'
+        try:
+            params, pcov, inf, msg, ier =\
+                opt.curve_fit(func, xdata, ydata, p0,
+                            sigma=yerr, #absolute_sigma=True,
+                            full_output=1)
+        except RuntimeError as e:
+            info['ok'] = False
+            info['msg'] = str(e)
+        else:
+            info['ok'] = not np.any(np.isinf(pcov))
+            info['msg'] = msg
+            info['cov'] = pcov
+            info['res_var'] = (np.sum((func(xdata, *params)-ydata)**2 / yerr**2)
+                               / dof)
+            std = [np.sqrt(pcov[i][i]) for i in range(len(p0))]
+    else:
+        if pprint: print("    Method: scipy ODR")
+        info['method'] = 'odr'
+        # change function signature again
+        def odrf(B, x):
+            return fun(x, *B)
+        # run ODR
+        model = odr.Model(odrf)
+        data = odr.RealData(xdata, ydata, sx=xerr, sy=yerr)
+        regr = odr.ODR(data, model, beta0=p0)
+        out = regr.run()
+        # evaluate
+        info['ok'] = (1 <= out.info <= 3)
+        info['cov'] = out.cov_beta
+        info['res_var'] = out.res_var
+        info['msg'] = out.stopreason
+        params = out.beta
+        std = out.sd_beta
+
+    # append units
+    params = [p*u for p,u in zip(params, p0units)]
+    std = [s*u for s,u in zip(std, p0units)]
+    # pretty print
+    if pprint:
+        print("    successful: %s"%info['ok'])
+        if 'msg' in info:
+            print("   ", info['msg'])
+        if 'res_var' in info:
+            print("    Residual variance: %f"%info['res_var'])
+            p = stats.chi2.cdf(info['res_var']*dof, dof)
+            print("    Probability of fit: {:.1f}%".format((1-p)*100))
+        if params and std:
+            print("    Parameters:")
+            for n,p,s in zip(paramnames, params, std):
+                print("      %s: %s"%(n, fmtquant(p, s, unit=True)))
+    return params, std, info
+
+
+def distribute(xs, xerr):
+    """Add normal distributed error to all values in `xs`
+      according to `xerr`.  Rescale xerr to units of xs.
+    """
+    if isinstance(xs, pq.Quantity):
+        unit = xs.units
+        xs = xs.magnitude
+        assert isinstance(xerr, pq.Quantity)
+        xerr = xerr.rescale(unit).magnitude
+    else: unit = 1
+    dist = random.normal(xs, xerr)
+    return dist * unit
+
+def draw_samples(fun, xs, params, xerr=None, yerr=None):
+    """Calculate function values using `params` and then distribute
+      `xs` and `ys` according to `xerr` and `yerr`.
+
+      Returns: (xdata, ydata)
+    """
+    ys = fun(xs, *params)
+    if xerr: xs = distribute(xs, xerr)
+    if yerr: ys = distribute(ys, yerr)
+    return (xs, ys)
+
+def test_model(fun, p0, xs, xerr, yerr):
+    xdata, ydata = draw_samples(fun, xs, p0, xerr=xerr, yerr=yerr)
+    pinit = [p * random.normal(1, 0.1) for p in p0]
+    # fit
+    p1, std1, info1 = fit(fun, xdata, ydata, pinit,
+                          yerr=yerr, xerr=xerr)
+    # bootstrap
+    p2, std2, info2 = fit(fun, xdata, ydata, pinit,
+                          yerr=yerr, xerr=xerr,
+                          usebootstrap=True)
+    # compare
+    print("Comparison: %s <-> %s (showing deviation from model"
+          " parameter)"%(info1['method'], info2['method']))
+    for i in range(len(p0)):
+        print("    Parameter {:d}: {} <-> {} (model: {})".format(i,
+                fmtquant(np.absolute(p1[i]-p0[i])/std1[i]),
+                fmtquant(np.absolute(p2[i]-p0[i])/std2[i]),
+                fmtquant(p0[i])))
+    print()
+    # plot
+    import matplotlib.pyplot as plt
+    plt.figure()
+    if xerr or yerr:
+        plt.errorbar(xdata, ydata, xerr=xerr, yerr=yerr, fmt=' ', label='data')
+    else:
+        plt.plot(xdata, ydata, '.', label='data')
+    plt.plot(xs, fun(xs, *p0), label='model')
+    plt.plot(xs, fun(xs, *p1), label=info1['method'])
+    plt.plot(xs, fun(xs, *p2), label=info2['method'])
+    plt.legend(loc='best', numpoints=1)
+    plt.show()
+
+def test_linear():
+    a = -0.5 * pq.nm / pq.s
+    b = 5 * pq.nm
+    def f(xs, a, b):
+        return xs * a + b
+    xbase = np.linspace(0, 16, 101) * pq.s
+    test_model(f, (a,b), xbase, None, 1*pq.nm)
+    test_model(f, (a,b), xbase,
+               1/np.sqrt(2)*pq.s, 1/np.sqrt(2)*pq.nm)
+
+def test_exp():
+    a = 5 * pq.nm
+    b = -0.1 / pq.s
+    c = 0.5 * pq.nm
+    def f(xs, a, b, c):
+        return a * np.exp(b * xs) + c
+    xbase = np.linspace(0, 30, 101) * pq.s
+    test_model(f, (a,b,c), xbase, None, 0.5*pq.nm)
+    test_model(f, (a,b,c), xbase,
+               0.3 * pq.s, 0.3 * pq.nm)
+
+if __name__ == '__main__':
+    test_linear()
+    test_exp()
---
+++ fmt.py
@@ -0,0 +1,279 @@
+
+import io
+import math
+import numpy as np
+import quantities as pq
+
+import subprocess
+import os
+
+logten = math.log(10.0)
+# TODO: handle infinit values and uncertainties
+def fmtuncert(value, uncert=None,
+              decimals=None, power=None, significance=None,
+              pm=' +- ', ten=' 10^', tex=False, paren=False,
+              nanempty=True):
+    """Format floating point values with uncertainty.
+      Does not work with numpy arrays.
+      Be aware of the rounding problem for the binary representation of values.
+
+      Arguments:
+          value, uncert: The floating point value and its uncertainty.
+              uncert may be None, then the plus-minus is omitted.
+          decimals: The number of digits to print after the decimal point.
+              When None it is set so that two significant digits of uncert
+              are visible, or four significant digits if uncert is zero or None.
+          power: The power to display the number in.  When non-zero the number
+              and its uncertainty are surrounded by parentheses.
+          significance: The value and error are rounded to this power of ten
+              before formatting (but after estimating power / decimals)
+          pm: The separator between the value and its uncertainty.
+          ten: The separator between the uncertainty (in parentheses) and
+              the power.
+          tex: If tex is True the power is surrounded by curly brackets.
+          paren: If True, surround text with parentheses when uncertainty
+              is shown, even if power is zero.
+          nanempty: If True, an empty string is returned when value or
+              uncert is np.nan
+
+      Returns:
+          A formatted string.
+    """
+    if nanempty and (np.isnan(value) or
+                     uncert is not None and np.isnan(uncert)):
+        return ""
+    if power is None:
+        if value == 0.0: power = 0
+        else: power = int(math.floor(math.log(abs(value))/logten))
+        if -2 <= power <= 3: power = 0
+    if decimals is None:
+        if not uncert:
+            # four significant digits
+            if value == 0.0: decimals = 1
+            else:
+                decimals = -int(math.floor(math.log(abs(value / 10**power))
+                                           / logten))
+                decimals = max(0, decimals+3)
+        else:
+            decimals = -int(math.floor(math.log(uncert / 10**power)/logten))
+            decimals = max(0, decimals+1)
+            if round(uncert / 10**power * 10**decimals) >= 100.0:
+                decimals -= 1
+    decimals = max(0, decimals)
+    if significance is not None:
+        s = 10**significance
+        value = round(value / s) * s
+        if uncert is not None:
+            uncert = round(uncert / s) * s
+    if uncert is None:
+        num = "{0:.{1}f}".format(value / 10**power, decimals)
+    else:
+        num = "{0:.{2}f}{3}{1:.{2}f}".format(
+            value / 10**power,
+            uncert / 10**power,
+            decimals, pm)
+    if uncert is not None and (power != 0 or paren):
+        num = "("+num+")"
+    if power != 0:
+        if tex: fmt = "{}{}{{{:d}}}"
+        else: fmt = "{}{}{:d}"
+        num = fmt.format(num, ten, power)
+    return num
+
+
+def fmtquant(quant, *args, **kwargs):
+    """Give python Quantity object (or UncertainQuantity) and any
+      arguments that fmtuncert takes.
+
+      Additional arguments:
+        unit: Set True to append unit (will make parentheses around
+          values).  Formats as tex formula when `tex=True` is set.
+          Defaults to True.
+    """
+    tex = kwargs.get('tex', False)
+    unit = kwargs.pop('unit', True)
+    # value
+    quant = 1 * quant
+    value = quant.magnitude
+    # uncert
+    uncert = None
+    if len(args) > 0:
+        uncert = args[0]
+        args = args[1:]
+    elif 'uncert' in kwargs:
+        uncert = kwargs.pop('uncert')
+    if uncert is not None:
+        if isinstance(uncert, pq.UncertainQuantity):
+            raise ValueError("Cannot format UncertainQuantity"
+                             " as uncertainty.")
+        if isinstance(uncert, pq.Quantity):
+            uncert = uncert.rescale(quant.units).magnitude
+    elif hasattr(quant, 'uncertainty'):
+        uncert = quant.uncertainty
+    else:
+        uncert = None
+    # fmt
+    kwargs.pop('paren', False)
+    s = fmtuncert(value, uncert, *args, paren=True, **kwargs)
+    if unit and quant.dimensionality != pq.dimensionless:
+        if tex: s = s + ' ' + quant.dimensionality.latex[1:-1]
+        else: s = s + ' ' + repr(quant.units)[13:]
+    return s
+
+def printtable(columns, caption="", tableno=1, filename=None,
+                columnformat=None, index=[]):
+    """Shorthand for formatting and printing table.
+      `filename` defaults to 'tableTABLENO.pdf'.
+    """
+    tab = fmttable(columns, caption, tableno, columnsformat, index)
+    if name is None: name = "table{}".format(tableno)
+    printtex(filename, tab)
+
+def fmttable(columns, tableno=1, caption="",
+             columnformat=None, index=[],
+             nanempty=True):
+    """Format data as tex string threeparttable.
+
+      Arguments:
+        columns: [
+            (heading, values, uncert, decimals, power=0, significance=None),
+            (heading, values, fun=None)
+          ]
+        tableno: Numbering of table, defaults to 1.
+        caption: Caption typesetted below table.
+        columnformat:  List of 'r', 'c' or 'l' giving the text alignment
+          in the table cells for every column.  Don't forget alignment of
+          index column if you didn't set it to None.  Defaults to all
+          right aligned.
+        index: The index put into the first column.  Has to have the same length
+          as the longest column.  Defaults to an enumeration (when set to empty
+          list).  No index is printed when `index is None`.
+        nanempty: Passed on to fmtuncert(), defaults to True.
+
+      Returns: a string.
+    """
+    coln = len(columns) # number of cols excluding index
+    colN = coln+1 if index is not None else coln # and including index
+    rown = max(len(col[1]) for col in columns)
+    # create enumerating index or check given one
+    if index == []: index = range(1, rown+1)
+    assert index is None or len(index) == rown
+    # create right aligned column format or check given one
+    if not columnformat:
+        columnformat = 'r' * (colN)
+    assert len(columnformat) == colN
+
+    # format cells to strings
+    fmt = np.empty( (rown, coln), dtype=str)
+    for coli, col in enumerate(columns):
+        heading = col[0]
+        if 2 <= len(col) <= 3: # object / string values
+            fun = col[2] if len(col) == 3 else repr
+            for rowi, dat in enumerate(col[1]):
+                fmt[rowi, coli] = fun(dat)
+        elif 4 <= len(col) <= 6: # number values
+            values = col[1]
+            uncert = col[2]
+            decimals = col[3]
+            power = col[4] if len(col) >= 5 else 0
+            signific = col[5] if len(col) >= 6 else None
+            if isinstance(values, pq.Quantity):
+                assert isinstance(uncert, pq.Quantity)
+                if uncert.units != values.units:
+                    print("INFO: rescaling uncertainty for", heading)
+                    uncert = uncert.rescale(values.units)
+                uncert = uncert.magnitude
+                values = values.magnitude
+            else:
+                assert not isinstance(uncert, pq.Quantity)
+            if isinstance(uncert, np.ndarray):
+                if uncert.size == 1:
+                    print("INFO: broadcasting uncertainty for", heading)
+                    uncert = np.array([np.asscalar(uncert)] * len(values))
+                if uncert.size != len(values):
+                    raise ValueError("Dimension mismatch for %s"%heading)
+            elif not isinstance(uncert, list):
+                print("INFO: broadcasting uncertainty for", heading)
+                uncert = np.array([uncert] * len(values))
+            elif len(uncert) != len(values):
+                raise ValueError("Dimension mismatch for %s"%heading)
+            for rowi, (v, u) in enumerate(zip(value, uncert)):
+                fmt[rowi][coli] = '$' + fmtuncert(v, u, decimals,
+                                            power, signific,
+                                            pm=r"\pm", tex=True,
+                                            nanempty=nanempty) + '$'
+        else:
+            raise ValueError("Bad tuple for column %d"%(coli+1))
+
+    # build string
+    NL = '\n'
+    s = StringIO()
+    s.write(r"""
+\setcounter{table}{%d}
+\begin{table}
+\centering
+\begin{threeparttable}
+\begin{tabular}{%s}
+\toprule
+"""%(tableno-1, columnformat))
+    # header
+    headings = [a[0] for a in columns]
+    f.write("{} &" + " & ".join(headings) + r"\\" + NL)
+    # data
+    for rowi in range(rown):
+        if index is not None:
+            s.write(repr(index[row]))
+        for coli in range(coln):
+            s.write(" & ")
+            s.write(fmt[rowi][coli])
+        s.write(r" \\" + NL)
+    # outro
+    caption = r"\caption{%s}"%caption if caption else ""
+    s.write(r"""\bottomrule
+\end{tabular}
+%s
+\end{threeparttable}
+\end{table}
+"""%(caption))
+    return s.getvalue()
+
+
+def printtex(name, tex):
+    """Put tex into file surrounded by some document definitions.
+      Then compile it using pdflatex.  Deletes intermediary files
+      when compilation was successful.
+
+      Arguments:
+        name: Basename of file to write.
+          Files with extensions `.tex, .log, .aux, .pdf` are
+          created / used.
+        tex: The tex as string.
+
+      Returns: True if compilation completed successfully.
+    """
+    texfile = name + '.tex'
+    pdffile = name + '.pdf'
+    # write file
+    with open(texfile, 'w+') as f:
+        NL = '\n'
+        f.write(r"""
+\documentclass[a4paper]{scrartcl}
+\usepackage{microtype, lmodern, ngerman}
+\usepackage{amsmath, esvect, booktabs, threeparttable}
+\begin{document}"""+NL)
+        f.write(tex)
+        f.write(NL+r"\end{document}"+NL)
+
+    # compile tex
+    print("Compiling to "+pdffile)
+    proc = subprocess.Popen(['pdflatex', texfile])
+    proc.communicate()
+    res = proc.returncode
+    if res != 0:
+        print("Compilation failed, return code: %d"%res)
+        return False
+    else:
+        os.unlink(filename+'.log')
+        os.unlink(filename+'.tex')
+        os.unlink(filename+'.aux')
+        return True
