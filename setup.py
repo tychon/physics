c71ab63a555afb02859412b902eb59dd07d3c4eb
tychon <tychon@tychons.net>
2016-05-05 00:15:00 +0200

    package with setuptools, add README

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='physics',
+      version='0.1',
+      description='Some helper functions for fitting with quantities and'
+                  'formatting uncertainties.',
+      url='http://tychons.net/gitweb/?p=physics.git;a=tree',
+      author='Hannes Riechert',
+      author_email='tychon@tychons.net',
+      license='MIT',
+      packages=['physics'],
+      zip_safe=False)
